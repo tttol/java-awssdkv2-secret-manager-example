f7a6ee26b1bb4acd7c28b5e06592b4bd7adfa0b3
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-04-06 15:47:38 +0900

    Add AWS SDK Secrets Manager dependency and update IndexController

--- src/main/java/io/github/tttol/javaawssdkv2secretmanagerexample/controller/IndexController.java
+++ src/main/java/io/github/tttol/javaawssdkv2secretmanagerexample/controller/IndexController.java
@@ -3,8 +3,14 @@ package io.github.tttol.javaawssdkv2secretmanagerexample.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.tttol.javaawssdkv2secretmanagerexample.service.AwsService;
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class IndexController {
+    private final AwsService awsService;
+    
     @GetMapping("/sample")
     public String sample() {
         return "Hello Secret Manager Example!";
@@ -12,8 +18,7 @@ public class IndexController {
 
     @GetMapping("/get")
     public String index() {
-        
-        return "Hello Secret Manager Example!";
+        return awsService.getSecret(); 
     }  
 
 }
---
+++ src/main/java/io/github/tttol/javaawssdkv2secretmanagerexample/service/AwsService.java
@@ -0,0 +1,47 @@
+package io.github.tttol.javaawssdkv2secretmanagerexample.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+import software.amazon.awssdk.services.secretsmanager.model.SecretsManagerException;
+
+@Service
+@Slf4j
+/**
+ * AwsService
+ *
+ * https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/javav2/example_code/secretsmanager/src/main/java/com/example/secrets/GetSecretValue.java
+ */
+public class AwsService {
+    public String getSecret() {
+        var secretName = "sample";
+        var region = Region.AP_NORTHEAST_1;
+        SecretsManagerClient secretsClient = SecretsManagerClient.builder()
+                .region(region)
+                .build();
+
+        var value = getValue(secretsClient, secretName);
+        secretsClient.close();    
+        return value;
+    }
+
+    public String getValue(SecretsManagerClient secretsClient, String secretName) {
+        try {
+            GetSecretValueRequest valueRequest = GetSecretValueRequest.builder()
+                    .secretId(secretName)
+                    .build();
+
+            GetSecretValueResponse valueResponse = secretsClient.getSecretValue(valueRequest);
+            var secret = valueResponse.secretString();
+            return secret;
+
+        } catch (SecretsManagerException e) {
+            log.error("error message={}", e.awsErrorDetails().errorMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+}
